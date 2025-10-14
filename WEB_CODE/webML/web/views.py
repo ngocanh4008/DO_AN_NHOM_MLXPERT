556c5e6dea61d05f3beb7162b68b16aae560f2ca
Trần Trinh <145744531+TranTrinhMavis273@users.noreply.github.com>
2025-10-14 10:53:46 +0700

    thêm ghi chú cho file views.pu

--- WEB_CODE/webML/web/views.py
+++ WEB_CODE/webML/web/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'web/home.html')
+    return render(request, 'web/home.html') #hàm để tạo lệnh hiển thị trang khi truy cập vào url tương ứng đã cấu hình trong file urls.py
 def login(request):
     return render(request, 'web/login.html')
