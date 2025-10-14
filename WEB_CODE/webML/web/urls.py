55ef44c05b7340ad82bb64ac44bbec8f0ea2c367
Trần Trinh <145744531+TranTrinhMavis273@users.noreply.github.com>
2025-10-14 10:52:18 +0700

    thêm ghi chú cho file urls.py

--- WEB_CODE/webML/web/urls.py
+++ WEB_CODE/webML/web/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home,),
-    path('login', views.login,)
+    path('', views.home,), # phần '' để cấu hình đường dẫn cho trang. Ví dụ ở đâu '' trống --> runserver sẽ hiện ra trang này đầu tiên
+    path('login', views.login,) # 'login' có nghĩa là đường dẫn của trang sẽ là "....:8000/login" 
+    #views.[tên trang] --> để hiển thị trang tương ứng với đường dẫn trong ''
 ]
+
