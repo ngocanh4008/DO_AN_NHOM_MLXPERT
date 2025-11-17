e143ce5d892886dc3ecd33bf4f6142239b241ed9
ngkhanhdu <dunk23416@st.uel.edu.vn>
2025-11-17 22:38:48 +0700

    Xóa các phần code test

---
+++ web/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.login_view),
+    path('home', views.home),
+    path('price', views.price_view),
+    path('report/', views.report_view, name='report'),
+    path('overview/', views.overview_view, name='overview'),
+    path('api/reports', views.api_reports, name='api_reports'),
+    path('api/download_report/', views.download_report, name='download_report'),
+    path('api/reports/<int:report_id>', views.api_delete_report, name='api_delete_report'),
+    path('reports/redownload/<int:report_id>', views.api_redownload_report, name='api_redownload_report'),
+]
